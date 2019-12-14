e2182fa2091d7d2e5317d9c5b70af03d4717d2fa
Troy Thompson <troycthompson@gmail.com>
2019-12-13 16:22:35 -0800

    Shortcut way

--- main.go
+++ main.go
@@ -10,8 +10,13 @@ func main() {
 
 	// Assigning a value during initialization
 	var j int = 43
+
+	// Let go figure out the type
+	k := 44
 	fmt.Println("Default way:")
 	fmt.Println(i)
 	fmt.Println("Same line way:")
 	fmt.Println(j)
+	fmt.Println("Shortcut way:")
+	fmt.Println(k)
 }
